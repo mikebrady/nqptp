337da3c0d45cb82b5321ac3dd9bdff412074cce3
Mike Brady <4265913+mikebrady@users.noreply.github.com>
2021-03-19 09:01:40 +0000

    Update nqptp-shm-structures.h

--- nqptp-shm-structures.h
+++ nqptp-shm-structures.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the nqPTP distribution (https://github.com/mikebrady/nqPTP).
+ * Copyright (c) 2021 Mike Brady.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Commercial licensing is also available.
+ */
+
 #ifndef NQPTP_SHM_STRUCTURES_H
 #define NQPTP_SHM_STRUCTURES_H
 
